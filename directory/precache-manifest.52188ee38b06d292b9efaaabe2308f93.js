7bba3a8ecab3b150532ef037298407994fc46adb
Rakesh Rai Hennadka <rakesh.rai.hennadka@verizonmedia.com>
2019-08-12 21:35:10 -0700

    Adding directory

---
+++ directory/precache-manifest.52188ee38b06d292b9efaaabe2308f93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d685991883cb6d938bd7fb45eeb8249b",
+    "url": "/directory/index.html"
+  },
+  {
+    "revision": "6964a2d43fefe0e2c14a",
+    "url": "/directory/static/css/main.87aa31c0.chunk.css"
+  },
+  {
+    "revision": "ecfba1de40a4f26bd71d",
+    "url": "/directory/static/js/2.59d05024.chunk.js"
+  },
+  {
+    "revision": "6964a2d43fefe0e2c14a",
+    "url": "/directory/static/js/main.a050e61f.chunk.js"
+  },
+  {
+    "revision": "2739d1caa8cf1b8e82a1",
+    "url": "/directory/static/js/runtime~main.606dd170.js"
+  }
+]);
